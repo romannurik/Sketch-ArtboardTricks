d4127d439877d89e2d657e05a82dd10c6d9aebfb
brianmoyano <brian_moyano@live.com>
2017-08-22 11:57:45 -0300

    Adding Magic

--- ArtboardTricks.sketchplugin/Contents/Sketch/both.js
+++ ArtboardTricks.sketchplugin/Contents/Sketch/both.js
@@ -1,6 +1,6 @@
 
-@import 'add_number_identifiers_to_artboards.cocoascript'
-@import 'rearrange_artboards_into_grid.cocoascript'
+@import 'add_number_identifiers_to_artboards.js'
+@import 'rearrange_artboards_into_grid.js'
 var onRun = function(context) {
   rearrangeArtboardsIntoGrid(context);
   prependNumbersToArtboards(context);
